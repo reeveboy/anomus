d702032988b23760f39bd752b09072677ed02e64
reeveboy <reeveboy10@gmail.com>
2022-08-21 11:37:45 +0530

    index page ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Home: React.FC = () => {
-  return <div>Heloow</div>;
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <div className="text-3xl ">Want to have an Annoymous Disccusion?</div>
+      <p className="p-2"></p>
+      <div className="flex justify-between">
+        <button className="shadow bg-emerald-500 hover:bg-emerald-600 focus:shadow-outline focus:outline-none py-2 px-4 rounded">
+          Create a room
+        </button>
+        <p className="p-2"></p>
+        <button className="shadow bg-sky-500 hover:bg-sky-600 focus:shadow-outline focus:outline-none py-2 px-4 rounded">
+          Join a room
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
